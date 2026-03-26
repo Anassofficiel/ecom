2e8f643de011b616da4d5bfaad5a2a10b76f70ee
Anassofficiel <anassfatihi2026@gmail.com>
2026-03-26 12:57:40 +0100

    add robots.txt with sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -46,11 +46,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'daily',
       priority: 0.8,
     },
-    {
-      url: `${baseUrl}/checkout`,
-      lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 0.5,
-    },
   ]
 }
