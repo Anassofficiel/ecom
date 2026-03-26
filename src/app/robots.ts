0ab73be6507008fe3cd93ae9802b153d2731b6c9
Anassofficiel <anassfatihi2026@gmail.com>
2026-03-26 15:22:36 +0100

    add robots.txt with sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://veneziaelectro.vercel.app/sitemap.xml',
+    sitemap: 'https://venziaelectro.vercel.app/sitemap.xml',
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://veneziaelectro.vercel.app'
+  const baseUrl = 'https://veniziaelectro.vercel.app/'
 
   return [
     {
