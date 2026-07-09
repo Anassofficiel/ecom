73111d36e3a3081d7789ab35efb3f73babf76381
Anassofficiel <anassfatihi2026@gmail.com>
2026-07-09 07:33:47 +0100

    Fix old Vercel redirects and checkout SEO

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,27 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.20"],
   devIndicators: false,
 
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "veneziaelectro.vercel.app",
+          },
+        ],
+        destination: "https://electromostafa55.ma/:path*",
+        permanent: true,
+      },
+      {
+        source: "/checkout",
+        destination: "/",
+        permanent: false,
+      },
+    ];
+  },
+
   images: {
     remotePatterns: [
       {
