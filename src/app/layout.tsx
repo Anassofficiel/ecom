9272b71d8c77b17a30709b0392b5e00904cfb013
Anassofficiel <fatiihanass99@gmail.com>
2026-08-13 02:25:04 +0100

    add microsoft clarity tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,9 @@ const phone = "+212 658-416769"
 // Meta Pixel ID
 const META_PIXEL_ID = "24800348739620824"
 
+// Microsoft Clarity Project ID
+const CLARITY_PROJECT_ID = "y0wuicqrq7"
+
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
 
@@ -228,6 +231,35 @@ export default function RootLayout({
 
               fbq('init', '${META_PIXEL_ID}');
               fbq('track', 'PageView');
+            `,
+          }}
+        />
+
+        {/* Microsoft Clarity */}
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){
+                  (c[a].q=c[a].q||[]).push(arguments)
+                };
+
+                t=l.createElement(r);
+                t.async=1;
+                t.src="https://www.clarity.ms/tag/"+i;
+
+                y=l.getElementsByTagName(r)[0];
+                y.parentNode.insertBefore(t,y);
+
+              })(
+                window,
+                document,
+                "clarity",
+                "script",
+                "${CLARITY_PROJECT_ID}"
+              );
             `,
           }}
         />
