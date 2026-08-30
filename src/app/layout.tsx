56109710136f895951d361135093ef84088fc5e3
Anassofficiel <fatiihanass99@gmail.com>
2026-08-30 01:12:48 +0100

    fix meta pixel facebook browser issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -195,48 +195,50 @@ export default function RootLayout({
         {/* Meta Pixel */}
         <Script
           id="meta-pixel"
-          strategy="lazyOnload"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `
-      (function(f,b,e,v,n,t,s)
-      {
-        if(f.fbq) return;
+(function(f,b,e,v,n,t,s)
+{
+ if(window.fbq) return;
 
-        n=f.fbq=function(){
-          n.callMethod ?
-          n.callMethod.apply(n,arguments) :
-          n.queue.push(arguments)
-        };
+ n=f.fbq=function(){
+   n.callMethod ?
+   n.callMethod.apply(n,arguments) :
+   n.queue.push(arguments)
+ };
 
-        if(!f._fbq) f._fbq=n;
+ if(!f._fbq) f._fbq=n;
 
-        n.push=n;
-        n.loaded=true;
-        n.version='2.0';
-        n.queue=[];
+ n.push=n;
+ n.loaded=true;
+ n.version='2.0';
+ n.queue=[];
 
-        t=b.createElement(e);
-        t.async=true;
-        t.src=v;
+ t=b.createElement(e);
+ t.async=true;
+ t.src=v;
 
-        s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s);
+ s=b.getElementsByTagName(e)[0];
+ s.parentNode.insertBefore(t,s);
 
-      })(
-        window,
-        document,
-        'script',
-        'https://connect.facebook.net/en_US/fbevents.js'
-      );
+})(
+window,
+document,
+'script',
+'https://connect.facebook.net/en_US/fbevents.js'
+);
 
 try {
-  fbq('init','${META_PIXEL_ID}');
-  fbq('track','PageView');
 
-} catch(e) {
-  console.warn('Meta Pixel error:',e);
+ window.fbq('init','${META_PIXEL_ID}');
+ window.fbq('track','PageView');
+
 }
-    `,
+catch(e){
+ console.warn('Meta Pixel error',e);
+}
+`,
           }}
         />
 
